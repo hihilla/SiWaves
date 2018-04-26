2391cec2ba2499dd25e6799b6e68bc70a66d1323
Hilla Barkal <hilla.guz@gmail.com>
2018-04-27 02:30:17 +0300

    fix crash

--- app/src/main/java/com/example/hilla/siwaves/MainActivity.java
+++ app/src/main/java/com/example/hilla/siwaves/MainActivity.java
@@ -43,78 +43,81 @@ public class MainActivity extends AppCompatActivity {
         textview.getPaint().setShader( myShader );
     }
 
-    private void requestSong() {
-        TextView textView = (TextView) findViewById(R.id.text);
-        String songName = textView.getText().toString();
-        songName.replaceAll(" ", "+");
-        String urlString = "http://18.218.124.172:4000/get_song?word_search=" + songName;
-        HttpURLConnection urlConnection = null;
-        try {
-            URL url = new URL(urlString);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            String jsonString = convertStreamToString(in);
-            JSONObject obj = new JSONObject(jsonString);
-            if (!obj.get("status").toString().equals("SUCCESS")) {
-                // bad response!!!
-                return;
-            }
-            List<Integer> amplitudes = parseInts(obj.getJSONArray("amplitudes"));
-            List<Long> timing = parseLongs(obj.getJSONArray("timing"));
-            String title = obj.getString("title");
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-        }
-    }
-
-    private List<Integer> parseInts(JSONArray jsonArray) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        if (jsonArray != null) {
-            int len = jsonArray.length();
-            for (int i = 0; i < len; i++) {
-                try {
-                    list.add((Integer) jsonArray.get(i));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return list;
-    }
-
-    private List<Long> parseLongs(JSONArray jsonArray) {
-        ArrayList<Long> list = new ArrayList<Long>();
-        if (jsonArray != null) {
-            int len = jsonArray.length();
-            for (int i = 0; i < len; i++) {
-                try {
-                    list.add((Long) jsonArray.get(i));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return list;
-    }
-
-    private String convertStreamToString(InputStream is) {
-        Scanner s = new Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
-    }
+//    private void requestSong() {
+//        TextView textView = (TextView) findViewById(R.id.text);
+//        String songName = textView.getText().toString();
+//        songName.replaceAll(" ", "+");
+//        String urlString = "http://18.218.124.172:4000/get_song?word_search=" + songName;
+//        HttpURLConnection urlConnection = null;
+//        try {
+//            URL url = new URL(urlString);
+//            urlConnection = (HttpURLConnection) url.openConnection();
+//            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+//            String jsonString = convertStreamToString(in);
+//            JSONObject obj = new JSONObject(jsonString);
+//            if (!obj.get("status").toString().equals("SUCCESS")) {
+//                // bad response!!!
+//                return;
+//            }
+//            List<Integer> amplitudes = parseInts(obj.getJSONArray("amplitudes"));
+//            List<Long> timing = parseLongs(obj.getJSONArray("timing"));
+//            String title = obj.getString("title");
+//
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (urlConnection != null) {
+//                urlConnection.disconnect();
+//            }
+//        }
+//    }
+//
+//    private List<Integer> parseInts(JSONArray jsonArray) {
+//        ArrayList<Integer> list = new ArrayList<Integer>();
+//        if (jsonArray != null) {
+//            int len = jsonArray.length();
+//            for (int i = 0; i < len; i++) {
+//                try {
+//                    list.add((Integer) jsonArray.get(i));
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//        return list;
+//    }
+//
+//    private List<Long> parseLongs(JSONArray jsonArray) {
+//        ArrayList<Long> list = new ArrayList<Long>();
+//        if (jsonArray != null) {
+//            int len = jsonArray.length();
+//            for (int i = 0; i < len; i++) {
+//                try {
+//                    list.add((Long) jsonArray.get(i));
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//        return list;
+//    }
+//
+//    private String convertStreamToString(InputStream is) {
+//        Scanner s = new Scanner(is).useDelimiter("\\A");
+//        return s.hasNext() ? s.next() : "";
+//    }
 
 
     public void onSearchClicked(View view) {
-        requestSong();
+//        requestSong();
+        TextView textView = (TextView) findViewById(R.id.text);
+        String songName = textView.getText().toString();
         Intent intent = new Intent(this, PlayActivity.class);
+        intent.putExtra("song name", songName);
         startActivity(intent);
     }
 }
